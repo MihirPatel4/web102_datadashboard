18d4c938f56c6ad006d954239db910e32632ad87
Mihir Patel <mk04patel@gmail.com>
2026-07-03 13:12:20 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState, useEffect } from 'react';
+import heroImg from './assets/hero.png';
+import './App.css';
+
+const App = () => {
+  return (
+    <div></div>
+  );
+};
+
+export default App;
