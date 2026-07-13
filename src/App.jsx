e8b63deef5b4cb9013b45c535bf7bdfe1bcea278
Mihir Patel <mk04patel@gmail.com>
2026-07-12 22:32:43 -0500

    Added brewery type chart

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import { useState, useEffect } from 'react';
 import { Link } from 'react-router';
+
+import { Bar, BarChart, CartesianGrid, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
+
 import './App.css';
 
 const App = () => {
   const [list, setList] = useState([]);
-  const [location, setLocation] = useState("los_angeles")
+  const [currentLocation, setCurrentLocation] = useState("los_angeles")
   const [searchInput, setSearchInput] = useState("");
   const [filter, setFilter] = useState("none");
   const [filteredList, setFilteredList] = useState([]);
   const [currentMode, setCurrentMode] = useState("");
   const [currentTypeCount, setCurrentTypeCount] = useState(0);
+  const [currentFreqData, setCurrentFreqData] = useState([]);
 
   useEffect(() => {
     fetchData("los_angeles");
@@ -19,6 +23,8 @@ const App = () => {
     const response = await fetch(`https://api.openbrewerydb.org/v1/breweries/?&by_city=${location}`);
     const json = await response.json();
 
+    setCurrentLocation(location);
+
     const notClosed = json.filter(brewery => brewery.brewery_type !== "closed");
     setList(notClosed);
     setFilteredList(notClosed);
@@ -26,6 +32,12 @@ const App = () => {
     //find type count and mode in the list
     const freqMap = {};
     notClosed.forEach(brewery => freqMap[brewery.brewery_type] = (freqMap[brewery.brewery_type] || 0) + 1);
+
+    const newFreqData = [];
+    for (const [key, value] of Object.entries(freqMap)) {
+      newFreqData.push({type: key, Count: value});
+    }
+    setCurrentFreqData(newFreqData);
 
     const count = Object.keys(freqMap).length;
     setCurrentTypeCount(count);
@@ -55,8 +67,6 @@ const App = () => {
     }
   };
 
-  // add 2 charts to the dashboard
-  //    brewery type, compare brewery count between any 2 cities (user input)
   return (
     <div className="dashboard">
       <div className="filters">
@@ -81,6 +91,20 @@ const App = () => {
         <p>Results found: {filteredList.length}</p>
         <p>Unique establishment types: {currentTypeCount}</p>
         <p>Most common establishment type: {currentMode}</p>
+      </div>
+      <div className="charts">
+        <div className="type-chart">
+          <h3>Brewery Type Frequency</h3>
+          <ResponsiveContainer width="100%" height={300}>
+            <BarChart data={currentFreqData}>
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="type" />
+              <YAxis />
+              <Tooltip />
+              <Bar dataKey="Count" fill="#8884d8" />
+            </BarChart>
+          </ResponsiveContainer>
+        </div>
       </div>
       <table className="brewery-table">
         <thead className="attribute-header">
